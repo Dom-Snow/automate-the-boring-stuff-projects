561cd1eef996d12d0f62bb7368244ef9298e5706
Dom-Snow <63740889+Dom-Snow@users.noreply.github.com>
2021-02-12 12:27:44 +0000

    Create Multiplication_Table_Maker.py

---
+++ Multiplication_Table_Maker.py
@@ -0,0 +1,21 @@
+import openpyxl
+from openpyxl.styles import Font
+font_title = Font(bold=True)
+
+math_tab = openpyxl.Workbook()
+sheet1 = math_tab.active
+sheet1.title = "math_tab"
+
+N = 7
+sheet1.row_dimensions[1].height = 15
+sheet1.column_dimensions["A"].width = 5
+
+for i in range(1, N):
+    sheet1.cell(row=1, column=i+1).value = i
+    sheet1.cell(row=1, column=i + 1).font = font_title
+    sheet1.cell(row=i+1, column=1).value = i
+    sheet1.cell(row=i + 1, column=1).font = font_title
+
+for i in range(2, N+1):
+    for j in range(2, N+1):
+        sheet1.cell(row=i, column=j).value = (i-1) * (j-1)
